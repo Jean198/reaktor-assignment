deceaa770af00ac208096c5cca91b46437aaa52f
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-18 13:53:52 +0200

    testing the deploy version, change the url endpoint

--- src/components/PilotsList.js
+++ src/components/PilotsList.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from "react";
 import Pilot from "./Pilot";
-import birdnest from "../assets/images/birdnest.png";
+import birdnest from '../assets/images/birdnest.png'
+import {URL} from '../App'
 
 const PilotsList = () => {
   const [pilots, setPilots] = useState([]);
   const [drone, setDrone] = useState([]);
   const txml = require("txml");
 
+
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch("https://assignments.reaktor.com/birdnest/drones")
+      fetch(`${URL}/drones`)
         .then((response) => response.text())
         .then((data) => {
+          //console.log (txml.simplify(txml.parse(data)))
           const droneData = txml.simplify(txml.parse(data)).report.capture
             .drone;
+          //console.log(droneData);
           setDrone(droneData);
           return droneData;
         })
         .then((data) => {
           return Promise.all(
             data.map((drone) =>
-              fetch("https://assignments.reaktor.com/birdnest/pilots/" + drone.serialNumber)
+              fetch(`${URL}/pilots/` + drone.serialNumber)
                 .then((resp) => resp.json())
                 .then((singlePilot) => {
                   return singlePilot;
@@ -30,15 +34,17 @@ const PilotsList = () => {
         })
         .then((pilotsList) => setPilots(pilotsList));
     }, 2000);
-    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="">
+
       <div>
-        <img src={birdnest} alt="" className="birdnest-image" />
+        <img src={birdnest} alt="" className="birdnest-image"/>
       </div>
       <Pilot pilots={pilots} />
+
+
     </div>
   );
 };
